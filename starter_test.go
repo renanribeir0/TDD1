332eed5e2174957bbae4780c2f3b433694d0aa65
renanribeir0 <renan.silva@sou.inteli.edu.br>
2024-08-12 10:39:50 -0300

    atualizando

--- starter_test.go
+++ starter_test.go
@@ -8,11 +8,11 @@ import (
 )
 
 func TestSayHello(t *testing.T) {
-	greeting := starter.SayHello("William")
-	assert.Equal(t, "Hello William. Welcome!", greeting)
+	greeting := starter.SayHello("Renan")
+	assert.Equal(t, "Hello Renan. Welcome!", greeting)
 
-	another_greeting := starter.SayHello("asdf ghjkl")
-	assert.Equal(t, "Hello asdf ghjkl. Welcome!", another_greeting)
+	another_greeting := starter.SayHello("Inteli")
+	assert.Equal(t, "Hello Inteli. Welcome!", another_greeting)
 }
 
 func TestOddOrEven(t *testing.T) {
@@ -25,4 +25,12 @@ func TestOddOrEven(t *testing.T) {
 		assert.Equal(t, "-45 is an odd number", starter.OddOrEven(-45))
 		assert.Equal(t, "-42 is an even number", starter.OddOrEven(-42))
 	})
+}
+
+func TestSayHelloAdditional(t *testing.T) {
+	greeting := starter.SayHello("William")
+	assert.Equal(t, "Hello William. Welcome!", greeting)
+
+	another_greeting := starter.SayHello("mundo")
+	assert.Equal(t, "Hello mundo. Welcome!", another_greeting)
 }
